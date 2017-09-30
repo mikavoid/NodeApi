eea1fa5b83148b8c7a490e38c24d23da623c8943
BOULAT Mickael <mikavoid@gmail.com>
2017-09-30 10:24:59 +0200

    Adding get todos and single todos

--- server.js
+++ server.js
@@ -3,9 +3,42 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+let todos = [
+    {
+        id: 1,
+        description: 'Meet mom for lunch',
+        completed: false
+    },
+    {
+        id: 2,
+        description: 'Go to market',
+        completed: false
+    },
+    {
+        id: 3,
+        description: 'Feed the cat',
+        completed: true
+    }
+]
+
 app.get('/', (req, res) => {
     res.status(200).json({data: 'Todo API root'})
 })
+
+app.get('/todos', (req, res) => {
+    res.status(200).json({data: todos})
+})
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    todos.map((todo) => {
+        if (todo.id == id) {
+            res.status(200).json({data: todo})
+        }
+    })
+    res.status(404).json({data: {error : 'Todo not found'}})
+})
+
 
 app.listen(PORT, () => {
     console.log("Server is listening on port " + PORT)
